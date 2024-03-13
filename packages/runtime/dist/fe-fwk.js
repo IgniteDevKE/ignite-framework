8518195e1f15850a547e63b49ce2113a6d8a88bc
Ian Chege <en20101832020@student.mut.ac.ke>
2024-03-13 18:19:41 +0300

    fix: Update package versions and fix import order

--- packages/runtime/dist/fe-fwk.js
+++ packages/runtime/dist/fe-fwk.js
@@ -16,39 +16,186 @@ function removeEventListeners(listeners = {}, el) {
 }
 
 function withoutNulls(arr) {
-    return arr.filter((item) => item !== null && item !== undefined)
+  return arr.filter((item) => item != null);
 }
-console.log(null != undefined);
+function arraysDiff(oldArray, newArray) {
+  return {
+    added: newArray.filter((newItem) => !oldArray.includes(newItem)),
+    removed: oldArray.filter((oldItem) => !newArray.includes(oldItem)),
+  };
+}
+const ARRAY_DIFF_OP = {
+  ADD: "add",
+  REMOVE: "remove",
+  MOVE: "move",
+  NOOP: "noop",
+};
+function arraysDiffSequence(
+  oldArray,
+  newArray,
+  equalsFn = (a, b) => a === b
+) {
+  const sequence = [];
+  const array = new ArrayWithOriginalIndices(oldArray, equalsFn);
+  for (let index = 0; index < newArray.length; index++) {
+    if (array.isRemoval(index, newArray)) {
+      sequence.push(array.removeItem(index));
+      index--;
+      continue;
+    }
+    if (array.isNoop(index, newArray)) {
+      sequence.push(array.noopItem(index));
+      continue;
+    }
+    const item = newArray[index];
+    if (array.isAddition(item, index)) {
+      sequence.push(array.addItem(item, index));
+      continue;
+    }
+    sequence.push(array.moveItem(item, index));
+  }
+  sequence.push(...array.removeItemsAfter(newArray.length));
+  return sequence;
+}
+class ArrayWithOriginalIndices {
+  #array = [];
+  #originalIndices = [];
+  #equalsFn;
+  constructor(array, equalsFn) {
+    this.#array = [...array];
+    this.#originalIndices = array.map((_, i) => i);
+    this.#equalsFn = equalsFn;
+  }
+  get length() {
+    return this.#array.length;
+  }
+  originalIndexAt(index) {
+    return this.#originalIndices[index];
+  }
+  findIndexFrom(item, fromIndex) {
+    for (let i = fromIndex; i < this.length; i++) {
+      if (this.#equalsFn(item, this.#array[i])) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  isRemoval(index, newArray) {
+    if (index >= this.length) {
+      return false;
+    }
+    const item = this.#array[index];
+    const indexInNewArray = newArray.findIndex((newItem) =>
+      this.#equalsFn(item, newItem)
+    );
+    return indexInNewArray === -1;
+  }
+  removeItem(index) {
+    const operation = {
+      op: ARRAY_DIFF_OP.REMOVE,
+      index,
+      item: this.#array[index],
+    };
+    this.#array.splice(index, 1);
+    this.#originalIndices.splice(index, 1);
+    return operation;
+  }
+  isNoop(index, newArray) {
+    if (index >= this.length) {
+      return false;
+    }
+    const item = this.#array[index];
+    const newItem = newArray[index];
+    return this.#equalsFn(item, newItem);
+  }
+  noopItem(index) {
+    return {
+      op: ARRAY_DIFF_OP.NOOP,
+      index,
+      originalIndex: this.originalIndexAt(index),
+      item: this.#array[index],
+    };
+  }
+  isAddition(item, fromIdx) {
+    return this.findIndexFrom(item, fromIdx) === -1;
+  }
+  addItem(item, index) {
+    const operation = {
+      op: ARRAY_DIFF_OP.ADD,
+      index,
+      item,
+    };
+    this.#array.splice(index, 0, item);
+    this.#originalIndices.splice(index, 0, -1);
+    return operation;
+  }
+  moveItem(item, toIndex) {
+    const fromIndex = this.findIndexFrom(item, toIndex);
+    const operation = {
+      op: ARRAY_DIFF_OP.MOVE,
+      originalIndex: this.originalIndexAt(fromIndex),
+      from: fromIndex,
+      index: toIndex,
+      item: this.#array[fromIndex],
+    };
+    const [_item] = this.#array.splice(fromIndex, 1);
+    this.#array.splice(toIndex, 0, _item);
+    const [originalIndex] = this.#originalIndices.splice(fromIndex, 1);
+    this.#originalIndices.splice(toIndex, 0, originalIndex);
+    return operation;
+  }
+  removeItemsAfter(index) {
+    const operations = [];
+    while (this.length > index) {
+      operations.push(this.removeItem(index));
+    }
+    return operations;
+  }
+}
 
 const DOM_TYPES = {
-    TEXT: "text",
-    ELEMENT: "element",
-    FRAGMENT: "fragment",
+  TEXT: "text",
+  ELEMENT: "element",
+  FRAGMENT: "fragment",
 };
 function h(tag, props = {}, children = []) {
-    return {
-        tag,
-        props,
-        children: mapTextNodes(withoutNulls(children)),
-        type: DOM_TYPES.ELEMENT,
-    };
+  return {
+    tag,
+    props,
+    children: mapTextNodes(withoutNulls(children)),
+    type: DOM_TYPES.ELEMENT,
+  };
 }
 function mapTextNodes(children) {
-    return children.map((child) =>
-        typeof child === "string" ? hString(child) : child
-    );
+  return children.map((child) =>
+    typeof child === "string" ? hString(child) : child
+  );
 }
 function hString(str) {
-    return {
-        type: DOM_TYPES.TEXT,
-        value: str,
-    }
+  return {
+    type: DOM_TYPES.TEXT,
+    value: str,
+  };
 }
 function hFragment(vNodes) {
-    return {
-        type: DOM_TYPES.FRAGMENT,
-        children: mapTextNodes(withoutNulls(vNodes)),
+  return {
+    type: DOM_TYPES.FRAGMENT,
+    children: mapTextNodes(withoutNulls(vNodes)),
+  };
+}
+function extractChildren(vdom) {
+  if (vdom.children == null) {
+    return [];
+  }
+  const children = [];
+  for (const child of vdom.children) {
+    if (child.type === DOM_TYPES.FRAGMENT) {
+      children.push(...extractChildren(child));
+    } else {
+      children.push(child);
     }
+  }
+  return children;
 }
 
 function destroyDOM(vdom) {
@@ -125,7 +272,10 @@ class Dispatcher {
 
 function setAttributes(el, attrs) {
   const { class: className, style, ...otherAttrs } = attrs;
-  if (className) setClass(el, className);
+  delete otherAttrs.key;
+  if (className) {
+    setClass(el, className);
+  }
   if (style) {
     Object.entries(style).forEach(([prop, value]) => {
       setStyle(el, prop, value);
@@ -133,15 +283,6 @@ function setAttributes(el, attrs) {
   }
   for (const [name, value] of Object.entries(otherAttrs)) {
     setAttribute(el, name, value);
-  }
-}
-function setClass(el, className) {
-  el.className = "";
-  if (typeof className === "string") {
-    el.className = className;
-  }
-  if (Array.isArray(className)) {
-    el.classList.add(...className);
   }
 }
 function setAttribute(el, name, value) {
@@ -154,57 +295,256 @@ function setAttribute(el, name, value) {
   }
 }
 function removeAttribute(el, name) {
-  el[name] = null;
+  try {
+    el[name] = null;
+  } catch {
+    console.warn(`Failed to set "${name}" to null on ${el.tagName}`);
+  }
   el.removeAttribute(name);
 }
+function setStyle(el, name, value) {
+  el.style[name] = value;
+}
+function removeStyle(el, name) {
+  el.style[name] = null;
+}
+function setClass(el, className) {
+  el.className = "";
+  if (typeof className === "string") {
+    el.className = className;
+  }
+  if (Array.isArray(className)) {
+    el.classList.add(...className);
+  }
+}
 
-function mountDOM(vdom, parentEl) {
-    switch (vdom.type) {
-        case DOM_TYPES.TEXT: {
-            createTextNode(vdom, parentEl);
-            break
-        }
-        case DOM_TYPES.ELEMENT: {
-            createElementNode(vdom, parentEl);
-            break
-        }
-        case DOM_TYPES.FRAGMENT: {
-            createFragmentNodes(vdom, parentEl);
-            break
-        }
-        default: {
-            throw new Error(`Can't mount DOM of type ${vdom.type}`)
-        }
+function mountDOM(vdom, parentEl, index) {
+  switch (vdom.type) {
+    case DOM_TYPES.TEXT: {
+      createTextNode(vdom, parentEl, index);
+      break;
     }
+    case DOM_TYPES.ELEMENT: {
+      createElementNode(vdom, parentEl, index);
+      break;
+    }
+    case DOM_TYPES.FRAGMENT: {
+      createFragmentNodes(vdom, parentEl, index);
+      break;
+    }
+    default: {
+      throw new Error(`Can't mount DOM of type ${vdom.type}`);
+    }
+  }
 }
-function createTextNode(vdom, parentEl) {
-    const { value } = vdom;
-    const textNode = document.createTextNode(value);
-    vdom.el = textNode;
-    parentEl.append(textNode);
+function insert(el, parentEl, index) {
+  if (index == null) {
+    parentEl.append(el);
+    return;
+  }
+  if (index < 0) {
+    throw new Error(`Index must be a positive integer, got ${index}`);
+  }
+  const children = parentEl.childNodes;
+  if (index >= children.length) {
+    parentEl.append(el);
+  } else {
+    parentEl.insertBefore(el, children[index]);
+  }
 }
-function createFragmentNodes(vdom, parentEl) {
-    const { children } = vdom;
-    vdom.el = parentEl;
-    children.forEach((child) => mountDOM(child, parentEl));
+function createTextNode(vdom, parentEl, index) {
+  const { value } = vdom;
+  const textNode = document.createTextNode(value);
+  vdom.el = textNode;
+  parentEl.append(textNode);
+  insert(textNode, parentEl, index);
 }
-function createElementNode(vdom, parentEl) {
-    const { tag, props, children } = vdom;
-    const element = document.createElement(tag);
-    addProps(element, props, vdom);
-    vdom.el = element;
-    children.forEach((child) => mountDOM(child, element));
-    parentEl.append(element);
+function createFragmentNodes(vdom, parentEl, index) {
+  const { children } = vdom;
+  vdom.el = parentEl;
+  children.forEach((child) => mountDOM(child, parentEl));
+  children.forEach((child, i) =>
+    mountDOM(child, parentEl, index ? index + i : null)
+  );
+}
+function createElementNode(vdom, parentEl, index) {
+  const { tag, props, children } = vdom;
+  const element = document.createElement(tag);
+  addProps(element, props, vdom);
+  vdom.el = element;
+  children.forEach((child) => mountDOM(child, element));
+  parentEl.append(element);
+  insert(element, parentEl, index);
 }
 function addProps(el, props, vdom) {
-    const { on: events, ...attrs } = props;
-    vdom.listeners = addEventListeners(el, events);
-    setAttributes(el, attrs);
+  const { on: events, ...attrs } = props;
+  vdom.listeners = addEventListeners(el, events);
+  setAttributes(el, attrs);
+}
+
+function areNodesEqual(nodeOne, nodeTwo) {
+  if (nodeOne.type !== nodeTwo.type) {
+    return false;
+  }
+  if (nodeOne.type === DOM_TYPES.ELEMENT) {
+    const { tag: tagOne } = nodeOne;
+    const { tag: tagTwo } = nodeTwo;
+    return tagOne === tagTwo;
+  }
+  return true;
+}
+
+function objectsDiff(oldObj, newObj) {
+  const oldKeys = Object.keys(oldObj);
+  const newKeys = Object.keys(newObj);
+  return {
+    added: newKeys.filter((key) => !(key in oldObj)),
+    removed: oldKeys.filter((key) => !(key in newObj)),
+    updated: newKeys.filter(
+      (key) => key in oldObj && oldObj[key] !== newObj[key]
+    ),
+  };
+}
+
+function isNotEmptyString(str) {
+  return str !== "";
+}
+function isNotBlankOrEmptyString(str) {
+  return isNotEmptyString(str.trim());
+}
+
+function patchDOM(oldVdom, newVdom, parentEl) {
+  if (!areNodesEqual(oldVdom, newVdom)) {
+    const index = Array.from(parentEl.childNodes).indexOf(oldVdom.el);
+    destroyDOM(oldVdom);
+    mountDOM(newVdom, parentEl, index);
+    return newVdom;
+  }
+  newVdom.el = oldVdom.el;
+  switch (newVdom.type) {
+    case DOM_TYPES.TEXT: {
+      patchText(oldVdom, newVdom);
+      return newVdom;
+    }
+    case DOM_TYPES.ELEMENT: {
+      patchElement(oldVdom, newVdom);
+      break;
+    }
+  }
+  patchChildren(oldVdom, newVdom);
+  return newVdom;
+}
+function patchChildren(oldVdom, newVdom) {
+  const oldChildren = extractChildren(oldVdom);
+  const newChildren = extractChildren(newVdom);
+  const parentEl = oldVdom.el;
+  const diffSeq = arraysDiffSequence(oldChildren, newChildren, areNodesEqual);
+  for (const operation of diffSeq) {
+    const { originalIndex, index, item } = operation;
+    switch (operation.op) {
+      case ARRAY_DIFF_OP.ADD: {
+        mountDOM(item, parentEl, index);
+        break;
+      }
+      case ARRAY_DIFF_OP.REMOVE: {
+        destroyDOM(item);
+        break;
+      }
+      case ARRAY_DIFF_OP.MOVE: {
+        const oldChild = oldChildren[originalIndex];
+        const newChild = newChildren[index];
+        const el = oldChild.el;
+        const elAtTargetIndex = parentEl.childNodes[index];
+        parentEl.insertBefore(el, elAtTargetIndex);
+        patchDOM(oldChild, newChild, parentEl);
+        break;
+      }
+      case ARRAY_DIFF_OP.NOOP: {
+        patchDOM(oldChildren[originalIndex], newChildren[index], parentEl);
+        break;
+      }
+    }
+  }
+}
+function patchText(oldVdom, newVdom) {
+  const el = oldVdom.el;
+  const { value: oldText } = oldVdom;
+  const { value: newText } = newVdom;
+  if (oldText !== newText) {
+    el.nodeValue = newText;
+  }
+}
+function patchElement(oldVdom, newVdom) {
+  const el = oldVdom.el;
+  const {
+    class: oldClass,
+    style: oldStyle,
+    on: oldEvents,
+    ...oldAttrs
+  } = oldVdom.props;
+  const {
+    class: newClass,
+    style: newStyle,
+    on: newEvents,
+    ...newAttrs
+  } = newVdom.props;
+  const { listeners: oldListeners } = oldVdom;
+  patchAttrs(el, oldAttrs, newAttrs);
+  patchClasses(el, oldClass, newClass);
+  patchStyles(el, oldStyle, newStyle);
+  newVdom.listeners = patchEvents(el, oldListeners, oldEvents, newEvents);
+}
+function patchAttrs(el, oldAttrs, newAttrs) {
+  const { added, removed, updated } = objectsDiff(oldAttrs, newAttrs);
+  for (const attr of removed) {
+    removeAttribute(el, attr);
+  }
+  for (const attr of added.concat(updated)) {
+    setAttribute(el, attr, newAttrs[attr]);
+  }
+}
+function patchClasses(el, oldClass, newClass) {
+  const oldClasses = toClassList(oldClass);
+  const newClasses = toClassList(newClass);
+  const { added, removed } = arraysDiff(oldClasses, newClasses);
+  if (removed.length > 0) {
+    el.classList.remove(...removed);
+  }
+  if (added.length > 0) {
+    el.classList.add(...added);
+  }
+  function toClassList(classes = "") {
+    return Array.isArray(classes)
+      ? classes.filter(isNotBlankOrEmptyString)
+      : classes.split(/(\s+)/).filter(isNotBlankOrEmptyString);
+  }
+}
+function patchStyles(el, oldStyle = {}, newStyle = {}) {
+  const { added, removed, updated } = objectsDiff(oldStyle, newStyle);
+  for (const style of removed) {
+    removeStyle(el, style);
+  }
+  for (const style of added.concat(updated)) {
+    setStyle(el, style, newStyle[style]);
+  }
+}
+function patchEvents(el, oldListeners = {}, oldEvents = {}, newEvents = {}) {
+  const { added, removed, updated } = objectsDiff(oldEvents, newEvents);
+  for (const eventName of removed.concat(updated)) {
+    el.removeEventListener(eventName, oldListeners[eventName]);
+  }
+  const addedListeners = {};
+  for (const eventName of added.concat(updated)) {
+    const listener = addEventListener(eventName, newEvents[eventName], el);
+    addedListeners[eventName] = listener;
+  }
+  return addedListeners;
 }
 
 function createApp({ state, view, reducers = {} }) {
   let parentEl = null;
   let vdom = null;
+  let isMounted = null;
   const dispatcher = new Dispatcher();
   const subscriptions = [dispatcher.afterEveryCommand(renderApp)];
   function emit(eventName, payload) {
@@ -221,18 +561,26 @@ function createApp({ state, view, reducers = {} }) {
     if (vdom) {
       destroyDOM(vdom);
     }
-    vdom = view(state, emit);
+    const newVdom = view(state, emit);
     mountDOM(vdom, parentEl);
+    vdom = patchDOM(vdom, newVdom, parentEl);
   }
   return {
     mount(_parentEl) {
+      if (isMounted) {
+        throw new Error("The application is already mounted");
+      }
       parentEl = _parentEl;
       renderApp();
+      vdom = view(state, emit);
+      mountDOM(vdom, parentEl);
+      isMounted = true;
     },
     unmount() {
       destroyDOM(vdom);
       vdom = null;
       subscriptions.forEach((unsubscribe) => unsubscribe());
+      isMounted = false;
     },
   };
 }
